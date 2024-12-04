7d754171663848aa581408b4cc889ffcf162ce7c
Kraafter <110327339+Kraafter@users.noreply.github.com>
2024-12-05 00:32:15 +0700

    switch to db

--- src/lib/urls-list.ts
+++ src/lib/urls-list.ts
@@ -1,4 +1,17 @@
-export const urls: {[key:string]:string} = {
-  "hello": "https://kraafter.me/",
-  "rhendra": "https://nerds.my.id/"
-}
+import { writable } from "svelte/store";
+// import { Redis } from "@upstash/redis";
+
+// export const redis = new Redis({
+//     url: import.meta.env.VITE_URL,
+//     token: import.meta.env.VITE_STORE,
+// })
+
+// const slugs = await redis.keys('*')
+// const values = await Promise.all(slugs.map(key => redis.get(key)))
+// const keyValuePairs = slugs.reduce((obj, key, index) => { obj[key] = values[index]; return obj; }, {});
+
+// let url = new Map(Object.entries(keyValuePairs));
+
+let url:Map<string,string> = new Map
+
+export const urls = writable(url)
