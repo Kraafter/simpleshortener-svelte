d67d3972dcc7bcbcc66aa84a32f4e63c535bcd5d
Kraafter <110327339+Kraafter@users.noreply.github.com>
2024-12-05 00:34:26 +0700

    use map instead of dict

--- src/routes/[slug]/+page.ts
+++ src/routes/[slug]/+page.ts
@@ -1,8 +1,11 @@
 import { urls } from '$lib/urls-list.js'
+var url:string
 
 export async function load({ params }) {
-	const slug = params.slug
-	const url = urls[slug]
+	urls.subscribe((value) => {
+		const slug = params.slug
+		url = value.get(slug)
+	})
 	return {
 		url
 	}
