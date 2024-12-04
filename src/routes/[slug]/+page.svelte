e3a0c8b53cf5f0b044a8f8cb5b86df106a6f3c87
Kraafter <110327339+Kraafter@users.noreply.github.com>
2024-12-05 00:34:06 +0700

    removed useless delay

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <svelte:head>
-	<meta http-equiv="refresh" content="3; url={redir}" />
+	<meta http-equiv="refresh" content="0; url={redir}" />
 </svelte:head>
 
-<p>Redirecting to {redir} in 3 seconds, click <a href="{redir}">here</a> if you are not being redirecting.</p>
+<p>Redirecting to {redir}, click <a href="{redir}">here</a> if you are not being redirecting.</p>
