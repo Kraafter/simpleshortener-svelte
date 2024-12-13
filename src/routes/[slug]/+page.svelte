9a8f855a6d2b53c5c18114d4880fc2e2b0b0b63e
Kraafter <110327339+Kraafter@users.noreply.github.com>
2024-12-13 16:41:51 +0700

    Updated function to use API

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,10 +1,41 @@
 <script lang="ts">
+	import { onMount } from 'svelte'
+
+	let entries = {}
 	export let data
-	const redir = data.url
+	var slug:string = data.slug
+
+	onMount(() => {
+
+		async function getList() {
+			try {
+				const response = await fetch('/api/list')
+				if (!response.ok) {
+					throw new Error('Network response was not ok')
+				}
+				entries = await response.json()
+				console.log(entries)
+			} catch (error) {
+				console.error('Fetch error:', error)
+			}
+		}
+
+		getList();
+
+	});
+	
 </script>
 
 <svelte:head>
-	<meta http-equiv="refresh" content="0; url={redir}" />
+	{#each Object.entries(entries) as [key, value]}  <!--quite an unconventional way to redirect IDK IM STUPID AND GAY-->
+        {#if key === slug}
+		<meta http-equiv="refresh" content="0; url='{ value }'" />
+		{/if}
+    {/each}
 </svelte:head>
 
-<p>Redirecting to {redir}, click <a href="{redir}">here</a> if you are not being redirecting.</p>
+{#each Object.entries(entries) as [key, value]}
+	{#if key === slug}
+	<p>Redirecting to {value}, click <a href={value}>here</a> if you are not being redirecting.</p>
+	{/if}
+{/each}
