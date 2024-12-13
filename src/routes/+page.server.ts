75883780f5d3bc0e5eded04affa3d4283d74d420
Kraafter <110327339+Kraafter@users.noreply.github.com>
2024-12-13 16:40:42 +0700

    Updated functions to add and delete slugs

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,24 +1,52 @@
-import { urls } from "$lib/urls-list.js";
-import { editLink, grabAllSlugs } from "$lib/index.js"
+import { fail, redirect } from '@sveltejs/kit';
+import type { Actions } from './$types';
+
 
 export const actions = {
-  upload: async ({ request }) => {
-    const form = await request.formData();
-    const key = form.get("accesskey");
-    const slug = form.get("slug");
-    const url = form.get("url");
-
-    editLink(key, slug, url)
-
-    const links = await grabAllSlugs()
-    console.log(links)
-
-    urls.subscribe((val) => {
-      val.clear()
-      for (var key of Object.keys(links)) {
-        val.set(key,links[key])
-      }
+    submitAdd: async ({ request, fetch }) => {
+      console.log("Sending request for new slug url pair")
+      const formData = await request.formData();
+      const accessa = formData.get('accessa');
+      const sluga = formData.get('sluga');
+      const urla = formData.get('urla');
       
-    })
-  }
-}
+      console.log('Adding')
+      const response = await fetch('/api/add', {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        method: 'POST',
+        body: JSON.stringify({ accessa, sluga, urla })
+      });
+
+      if (response.ok) {
+        return redirect(303, '/'); // Redirect on success
+      }
+
+      return fail(500, { error: 'Server error' });
+      
+    },
+
+    submitDel: async ({ request, fetch }) => {
+        console.log('Sending request to delete slug url pair')
+        const formData = await request.formData();
+        const accessd = formData.get('accessd');
+        const slugd = formData.get('slugd');
+
+        console.log('Deleting')
+        const response = await fetch('/api/delete', {
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          method: 'POST',
+          body: JSON.stringify({ accessd, slugd })
+        });
+
+        if (response.ok) {
+          return redirect(303, '/'); // Redirect on success
+        }
+  
+        return fail(500, { error: 'Server error' });
+
+      },
+  } satisfies Actions
