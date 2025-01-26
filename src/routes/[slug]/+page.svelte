520925a4fb5b562c4fa5d510cc98863f59860646
Kraafter <110327339+Kraafter@users.noreply.github.com>
2025-01-26 14:28:41 +0700

    Improved url gettind method

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,41 +1,54 @@
 <script lang="ts">
-	import { onMount } from 'svelte'
+    import { onMount } from 'svelte';
 
-	let entries = {}
-	export let data
-	var slug:string = data.slug
+    export let data;
+    let slug: string = data.slug;
 
-	onMount(() => {
+    console.log('Slug:', slug);
 
-		async function getList() {
-			try {
-				const response = await fetch('/api/list')
-				if (!response.ok) {
-					throw new Error('Network response was not ok')
-				}
-				entries = await response.json()
-				console.log(entries)
-			} catch (error) {
-				console.error('Fetch error:', error)
-			}
-		}
+    let linkGot: string;
+    let putHeader: string;
+    let putBody: string;
 
-		getList();
+    onMount(() => {
+        console.log('Component mounted');
 
-	});
-	
+        async function getLink() {
+            try {
+                const response = await fetch('/api/get', {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    method: 'POST',
+                    body: JSON.stringify({ slug })
+                });
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                } else {
+                    const jsonres = await response.json();
+                    console.log('Response JSON:', jsonres);
+                    if ((jsonres['status'] == 'OK') && jsonres['link'] != null) {
+                        linkGot = jsonres['link'];
+
+                        putHeader = `<meta http-equiv='refresh' content='1; url=${linkGot}' />`;
+                        putBody = `<p>Redirecting to ${linkGot}, click <a href="${linkGot}">here</a> if you are not being redirected.</p>`;
+                    } else {
+                        putBody = `<p>${slug} is not a valid shortened link, please check again</p>`;
+                    }
+                }
+            } catch (error) {
+                console.error('Fetch error:', error);
+            }
+        }
+
+        getLink();
+    });
 </script>
 
 <svelte:head>
-	{#each Object.entries(entries) as [key, value]}  <!--quite an unconventional way to redirect IDK IM STUPID AND GAY-->
-        {#if key === slug}
-		<meta http-equiv="refresh" content="0; url='{ value }'" />
-		{/if}
-    {/each}
+    {@html putHeader}
 </svelte:head>
 
-{#each Object.entries(entries) as [key, value]}
-	{#if key === slug}
-	<p>Redirecting to {value}, click <a href={value}>here</a> if you are not being redirecting.</p>
-	{/if}
-{/each}
+<main>
+    {@html putBody}
+</main>
